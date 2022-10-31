f79a8fad52745734ea320097c65c7aed049b16bc
이선영 <qrsty9636@gmail.com>
2022-10-31 23:18:40 +0900

    project setting 1. spring init 2. gitignore 3. swagger

---
+++ issuetracker-api/src/main/java/com/comento/issuetracker/IssuetrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.comento.issuetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuetrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IssuetrackerApiApplication.class, args);
+	}
+
+}
---
+++ issuetracker-api/src/test/java/com/comento/issuetracker/IssuetrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comento.issuetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuetrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
